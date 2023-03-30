f36bcd2b06ac0ed584fbb630d26748e50b8f4ca9
jeanlacosse <jeanlacosse@gmail.com>
2023-03-29 22:00:47 -0500

    calculator logic changed

--- src/components/Calculator.jsx
+++ src/components/Calculator.jsx
@@ -1,58 +1,64 @@
 import React, { useState } from 'react';
+import Buttons from './Buttons';
+import Display from './Display';
 
-function Calculator() {
-    const [num1, setNum1] = useState('');
-    const [num2, setNum2] = useState('');
-    const [operation, setOperation] = useState('');
-    const [result, setResult] = useState('');
+const Calculator = () => {
+    const [expression, setExpression] = useState('');
+    const [displayValue, setDisplayValue] = useState('');
 
-    const handleSubmit = async (event) => {
-        event.preventDefault();
-        const response = await fetch('/calculate', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({
-                num1: num1,
-                num2: num2,
-                operation: operation,
-            }),
-        });
-        const data = await response.json();
-        // this may need to change depending on how the response looks, it may also need to be turned into a number from a string
-        setResult(data.body)
+    const handleButtonClick = (buttonValue) => {
+        if (buttonValue === 'clear') {
+            setDisplayValue('');
+            setExpression('');
+        } else if (buttonValue === '+' || buttonValue === '-' || buttonValue === '*' || buttonValue === '/') {
+            setExpression(expression + buttonValue);
+            setDisplayValue(displayValue + buttonValue);
+        } else if (buttonValue === '=') {
+            const regex = /(\d+\.?\d*)([+\-*/])(\d+\.?\d*)/; // regex = regular expresssion, first () captures number with a decimal, second () captures the operation, and third () captures a second number
+            const match = expression.match(regex); // match() method is used to check if the regex expression is equal to the expression string, if not returns null, if so returns array of captured groups
+
+            if (match) {
+                // this will set the value of the variables using the matched array indicies from above
+                const num1 = parseFloat(match[1]);
+                const operator = match[2];
+                const num2 = parseFloat(match[3]);
+
+
+                const payload = {
+                    'num1': num1,
+                    'num2': num2,
+                    'operation': operator
+                };
+
+                console.log('payload is:', payload);
+
+                fetch('/calculate', {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json'
+                    },
+                    body: JSON.stringify(payload)
+                })
+                    .then(response => response.json())
+                    .then(data => {
+                        setDisplayValue(data.result);
+                        setExpression('');
+                    });
+            } else {
+                // Handle invalid expression
+            }
+        } else {
+            setExpression(expression + buttonValue);
+            setDisplayValue(displayValue + buttonValue);
+        }
     };
 
     return (
-        <div>
-            <form onSubmit={handleSubmit}>
-                <input
-                    type="number"
-                    value={num1}
-                    onChange={(e) => setNum1(e.target.value)}
-                />
-                <select
-                    name="operation"
-                    id="operation"
-                    value={operation}
-                    onChange={(e) => setOperation(e.target.value)}
-                >
-                    <option value="add">+</option>
-                    <option value="subtract">-</option>
-                    <option value="multiply">*</option>
-                    <option value="divide">/</option>
-                </select>
-                <input
-                    type="number"
-                    value={num2}
-                    onChange={(e) => setNum2(e.target.value)}
-                />
-                <button type='submit'>Calculate</button>
-            </form>
-            <div>{result}</div>
+        <div className="calculator">
+            <Display displayValue={displayValue} />
+            <Buttons handleButtonClick={handleButtonClick} />
         </div>
     );
-}
+};
 
 export default Calculator;
